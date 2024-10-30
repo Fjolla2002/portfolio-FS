e98a2e59f1e6bfca9b0e5c5f0f849d7008416ab1
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-10-30 21:56:37 +0100

    About page Add

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>AboutView</div>
+  <div class="p-20 flex flex-col items-start justify-start">
+    <h2 class="text-8xl text-baseWhite font-extrabold">About</h2>
+    <p class="py-10 max-w-[800px] text-lightGray text-left">
+      As an enthusiastic Software Devloper, I have a strong foundation in both
+      back-end and front-end development. I focus on creating efficient and
+      user-friendly applications, utilizing technologies such as HTML, CSS,
+      SCSS, JavaScript, and TypeScript. With proficiency in frameworks like
+      React and Vue.js, I work on implementing responsive designs and optimizing
+      user experiences. My knowledge of Node.js, Express.js, and .NET supports
+      the development of robust back-end systems. I bring strong problem-solving
+      skills and enjoy collaborating with my team to overcome challenges and
+      drive project success. Committed to continuous learning, I actively
+      explore new technologies and best practices to refine my skills and
+      contribute effectively to development teams.
+    </p>
+    <div class="flex items-center justify-start gap-10">
+      <div
+        class="bg-darkerGray p-4 rounded-lg flex flex-col items-start justify-between gap-2 text-lightGray text-2xl"
+      >
+        <span>
+          8
+          <span class="text-transparent bg-clip-text bg-toRightGradient">
+            +
+          </span>
+        </span>
+        <span class="text-transparent bg-clip-text bg-toRightGradient">
+          Projects
+        </span>
+      </div>
+      <div
+        class="bg-darkerGray p-4 rounded-lg flex flex-col items-start justify-between gap-2 text-lightGray text-2xl"
+      >
+        <span>
+          1
+          <span class="text-transparent bg-clip-text bg-toRightGradient">
+            +
+          </span>
+        </span>
+        <span class="text-transparent bg-clip-text bg-toRightGradient">
+          Years of experience
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
