84d98e079822183a74b34266ce9770ee85811a90
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-10-30 17:05:01 +0100

    Add Custom Cursor

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import BgCanvas from "./components/BgCanvas.vue";
+import CustomCursor from "./components/CustomCursor.vue";
 </script>
 
 <template>
+  <CustomCursor />
   <BgCanvas />
   <RouterView />
 </template>
---
+++ src/components/CustomCursor.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref } from "vue";
+
+const cursor = ref<HTMLDivElement | null>(null);
+
+const moveCursor = (event: MouseEvent) => {
+  if (cursor.value) {
+    cursor.value.style.left = `${event.clientX}px`;
+    cursor.value.style.top = `${event.clientY}px`;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("mousemove", moveCursor);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("mousemove", moveCursor);
+});
+</script>
+
+<template>
+  <div
+    ref="cursor"
+    class="fixed w-12 h-12 rounded-full pointer-events-none transform -translate-x-1/2 -translate-y-1/2 z-50 bg-[radial-gradient(circle,_white_0%,_#b0b0b0_50%,_#1a1a1a_100%)]"
+  ></div>
+</template>
