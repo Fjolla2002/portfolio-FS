b4db9a17deb66f3b43f8d7fe18877a795bd8d7b6
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-11-01 22:54:53 +0100

    4 projects data

---
+++ src/assets/data/work.ts
@@ -0,0 +1,198 @@
+import kh1 from "../projects/KosovaHike/bannerDesktop.png";
+import kh2 from "../projects/KosovaHike/eventsTrailDesktop.png";
+import kh3 from "../projects/KosovaHike/bannerEventsMobile.png";
+import kh4 from "../projects/KosovaHike/trailServicesMobile.png";
+import kh5 from "../projects/KosovaHike/allTrailsDesktop.png";
+import kh6 from "../projects/KosovaHike/allTrailsMobile.png";
+import kh7 from "../projects/KosovaHike/detail1.png";
+import kh8 from "../projects/KosovaHike/detail2.png";
+import kh9 from "../projects/KosovaHike/buddyDesktop.png";
+import kh10 from "../projects/KosovaHike/buddyMobile.png";
+import kh11 from "../projects/KosovaHike/gear1Desktop.png";
+import kh12 from "../projects/KosovaHike/gear2Desktop.png";
+import kh13 from "../projects/KosovaHike/gear1Mobile.png";
+import kh14 from "../projects/KosovaHike/gear2Mobile.png";
+import kh15 from "../projects/KosovaHike/incomingEvents.png";
+import kh16 from "../projects/KosovaHike/chatView.png";
+import kh17 from "../projects/KosovaHike/profileDesktop.png";
+import kh18 from "../projects/KosovaHike/profileMobile.png";
+
+import ac1 from "../projects/AtlasCard/banner.png";
+import ac2 from "../projects/AtlasCard/bannerMobile.png";
+import ac3 from "../projects/AtlasCard/navbarDesktop.png";
+import ac4 from "../projects/AtlasCard/navbarMobile.png";
+import ac5 from "../projects/AtlasCard/tasteMaker.png";
+import ac6 from "../projects/AtlasCard/tasteMakerMobile.png";
+import ac7 from "../projects/AtlasCard/highFlyer.png";
+import ac8 from "../projects/AtlasCard/highFlyerMobile.png";
+import ac9 from "../projects/AtlasCard/headTurner.png";
+import ac10 from "../projects/AtlasCard/headTurnerMobile.png";
+import ac11 from "../projects/AtlasCard/powerCouple.png";
+import ac12 from "../projects/AtlasCard/powerCoupleMobile.png";
+import ac13 from "../projects/AtlasCard/trulyPersonal.png";
+import ac14 from "../projects/AtlasCard/trulyPersonalMobile.png";
+import ac15 from "../projects/AtlasCard/executiveBrief.png";
+import ac16 from "../projects/AtlasCard/executiveBriefMobile.png";
+import ac17 from "../projects/AtlasCard/footer.png";
+import ac18 from "../projects/AtlasCard/footerMobile.png";
+
+import edma1 from "../projects/EDMA/home.png";
+import edma2 from "../projects/EDMA/homeMobile.png";
+import edma3 from "../projects/EDMA/aboutus.png";
+import edma4 from "../projects/EDMA/aboutusMobile.png";
+import edma5 from "../projects/EDMA/arch.png";
+import edma6 from "../projects/EDMA/archViewProject.png";
+import edma7 from "../projects/EDMA/services.png";
+import edma8 from "../projects/EDMA/projects.png";
+import edma9 from "../projects/EDMA/projectsMobile.png";
+import edma10 from "../projects/EDMA/contact.png";
+import edma11 from "../projects/EDMA/contactError.png";
+import edma12 from "../projects/EDMA/contactDe.png";
+import edma13 from "../projects/EDMA/contactMobile.png";
+import edma14 from "../projects/EDMA/moreView.png";
+import edma15 from "../projects/EDMA/login.png";
+import edma16 from "../projects/EDMA/menuMobile.png";
+
+import sp1 from "../projects/SneakPeak/home.png";
+
+import td1 from "../projects/TravelDream/home.png";
+import td2 from "../projects/TravelDream/about.png";
+import td3 from "../projects/TravelDream/tours.png";
+import td4 from "../projects/TravelDream/profile.png";
+import td5 from "../projects/TravelDream/cart.png";
+
+const work = [
+  {
+    id: 1,
+    title: "KosovaHike",
+    technologiesUsed:
+      " Node.js, Express.js, Typescript, MongoDB, React.js, SCSS, and Redux.",
+    status: "Finished",
+    links: [
+      {
+        name: "github",
+        links: [
+          "https://github.com/GzimAsani/hiking-frontend",
+          "https://github.com/GzimAsani/hiking-backend",
+        ],
+      },
+    ],
+    images: [
+      kh1,
+      kh2,
+      kh3,
+      kh4,
+      kh5,
+      kh6,
+      kh7,
+      kh8,
+      kh9,
+      kh10,
+      kh11,
+      kh12,
+      kh13,
+      kh14,
+      kh15,
+      kh16,
+      kh17,
+      kh18,
+    ],
+  },
+  {
+    id: 2,
+    title: "AtlasCard",
+    technologiesUsed: "React.js, SCSS.",
+    status: "Finished",
+    links: [
+      {
+        name: "github",
+        links: ["https://github.com/Fjolla2002/AtlasCard"],
+      },
+      {
+        name: "website",
+        links: ["https://atlascard-fs.netlify.app/"],
+      },
+    ],
+    images: [
+      ac1,
+      ac2,
+      ac3,
+      ac4,
+      ac5,
+      ac6,
+      ac7,
+      ac8,
+      ac9,
+      ac10,
+      ac11,
+      ac12,
+      ac13,
+      ac14,
+      ac15,
+      ac16,
+      ac17,
+      ac18,
+    ],
+  },
+  {
+    id: 3,
+    title: "EDMA GmbH",
+    technologiesUsed: "React.js, SCSS.",
+    status: "Finished",
+    links: [
+      {
+        name: "github",
+        links: ["https://github.com/Fjolla2002/EDMA"],
+      },
+      {
+        name: "website",
+        links: ["https://edma-fs.netlify.app/"],
+      },
+    ],
+    images: [
+      edma1,
+      edma2,
+      edma3,
+      edma4,
+      edma5,
+      edma6,
+      edma7,
+      edma8,
+      edma9,
+      edma10,
+      edma11,
+      edma12,
+      edma13,
+      edma14,
+      edma15,
+      edma16,
+    ],
+  },
+  {
+    id: 4,
+    title: "SneakPeak E-commerce",
+    technologiesUsed:
+      "Node.js, Express.js, Typescript, MongoDB, React.js, SCSS, and Redux.",
+    status: "Working",
+    images: [sp1],
+  },
+  {
+    id: 5,
+    title: "Travel Agency Website",
+    technologiesUsed: "HTML, CSS and Javascript.",
+    status: "Finished",
+    links: [
+      {
+        name: "github",
+        links: ["https://github.com/Fjolla2002/TravelAgency"],
+      },
+      {
+        name: "website",
+        links: ["https://travel-dream-eff.netlify.app/"],
+      },
+    ],
+    images: [td1, td2, td3, td4, td5],
+  },
+];
+
+export default work;
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,3 +1,6 @@
 <template>
-  <div>WorkView</div>
+  <div class="p-20 flex flex-col items-start justify-start w-full">
+    <h2 class="text-8xl text-baseWhite font-extrabold">Work</h2>
+    <div class="w-[80%] ml-auto grid grid-cols-2 gap-4 mt-20"></div>
+  </div>
 </template>
